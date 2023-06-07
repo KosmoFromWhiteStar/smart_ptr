13a66c57957840e68130191f8e15c8fb62adb448
Kosmo <The_Kosmo@outlook.com>
2023-06-07 16:12:24 +0500

    Сломал

--- incl/smartPtr.h
+++ incl/smartPtr.h
@@ -9,28 +9,30 @@ private:
 public:
 	Toy(std::string in_name) : name(in_name) {};
 	Toy() : Toy("Some Toy") {};
-	//Yavno ukazal
-	Toy(Toy* oth): name(oth->name){};
-
-	
+	Toy(Toy* toy) : name(toy->name) {};
 };
 
 class Shared_ptr_toy
 {
 private:
 	Toy* toy = nullptr;
-	int count = 0;
+	mutable int count = 0;
 	const int* count_this_toy = &count;
-	
 public:
-	Shared_ptr_toy() :toy(new Toy()) { count++; };
-	Shared_ptr_toy(std::string in_name) :toy(new Toy(in_name)) { count++; };
+	Shared_ptr_toy() :toy(new Toy()) {};
+	Shared_ptr_toy(std::string in_name) :toy(new Toy(in_name)) {};
 	Shared_ptr_toy& operator= (const Shared_ptr_toy& oth)
 	{
 		if (this == &oth)
 			return *this;
 		if (toy != nullptr) delete toy;
-		toy = new Toy(oth.toy);
+		toy = new Toy( oth.toy );
+		oth.count += 1;
+	}
+
+	int get_count()
+	{
+		return count;
 	}
 
 	void reset()
@@ -51,27 +53,38 @@ public:
 	}
 };
 
-Shared_ptr_toy* make_shared_ptr(std::string in_name) {
-	return new Shared_ptr_toy(in_name);
+Shared_ptr_toy make_shared_ptr(std::string in_name) {
+	Shared_ptr_toy temp (in_name);
+	return temp;
 }
 
-Shared_ptr_toy* make_shared_ptr(Shared_ptr_toy* toy) {
-	return new Shared_ptr_toy(*toy);
+Shared_ptr_toy make_shared_ptr(Shared_ptr_toy* toy) {
+	Shared_ptr_toy temp(*toy);
+	return temp;
+}
+
+Shared_ptr_toy make_shared_ptr() {
+	Shared_ptr_toy temp;
+	return temp;
 }
 
 class Box
 {
-	Shared_ptr_toy toys;
 public:
-	Box(Shared_ptr_toy* toy) : toys(*toy) {};
-	Box() : toys(nullptr) {};
+	Shared_ptr_toy toys;
+	Box(Shared_ptr_toy* toy) {
+		toys = *toy;
+	};
+	Box() {};
 };
 
 void ex1()
 {
-	Box box[2];
+	Shared_ptr_toy Ball = { "Ball" };
+	Box box[2] = { &Ball };
 
-
+	box[1].toys = make_shared_ptr( &Ball );
+	std::cout << Ball.get_count() << std::endl;
 }
 
 void do_ex(void(*ex)() = ex1)
